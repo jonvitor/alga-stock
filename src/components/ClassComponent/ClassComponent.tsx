44d95ea20dd26f85bc27b35f7849886f74b94432
João Vitor da Anunciação <joao.anunciacao@compasso.com.br>
2023-09-01 15:47:19 -0300

    cicle time component exemple

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 class ClassComponent extends React.Component {
+    constructor(props: any) {
+        super(props);
+        console.log('constructor reached!');
+    }
 
     state = {
         name: 'mundo'
     }
 
+    componentDidMount(): void {
+        console.log('componentDidMountReached!');
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate reached!');
+    }
+
     render() {
+        console.log('render reached!');
         return <div>
             <p>{this.state.name}</p>
             <button onClick={() => {
                 this.setState({ name: 'João' })
             }}>Click me</button>
-        </div>
+        </div>;
     }
 
 }
 
-export default ClassComponent
+export default ClassComponent;
