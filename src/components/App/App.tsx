38fe9588cba32444397f64dcbe74927795330f33
João Vitor da Anunciação <joao.anunciacao@compasso.com.br>
2023-09-13 18:35:40 -0300

    add container and move button to shared

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header';
-import Button from '../Button';
+import Button from '../../shared/Button';
+import Container from '../../shared/Container';
 
 function TestComponent() {
   return <img width="16" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK01qbQ5ju_5MxJ57JYaV3npMSsH_Wck7grctQ9Li17w&s" alt="search icon" />
@@ -12,14 +13,14 @@ function App() {
     <div className="App">
       <Header title='AlgaStock'/>
 
-      <div className="Container">
+      <Container>
         <Button 
           onClick={() => window.alert('Clicked')}
           appendIcon = {<TestComponent />}
         >
           Button
         </Button>
-      </div>
+      </Container>
     </div>
   );
 }
