77679419353e43ce467e77c690a69430e7c3a585
João Vitor da Anunciação <joao.anunciacao@compasso.com.br>
2023-09-13 18:11:38 -0300

    add button component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header';
+import Button from '../Button';
+
+function TestComponent() {
+  return <img width="16" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK01qbQ5ju_5MxJ57JYaV3npMSsH_Wck7grctQ9Li17w&s" alt="search icon" />
+}
 
 function App() {
   return (
     <div className="App">
       <Header title='AlgaStock'/>
+
+      <div className="Container">
+        <Button 
+          onClick={() => window.alert('Clicked')}
+          appendIcon = {<TestComponent />}
+        >
+          Button
+        </Button>
+      </div>
     </div>
   );
 }
