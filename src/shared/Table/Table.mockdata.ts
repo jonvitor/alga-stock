3df9ace339b593b4e89e8de4bc71e0dee4463182
João Vitor da Anunciação <joao.anunciacao@compasso.com.br>
2023-09-25 18:22:57 -0300

    Create mock data

---
+++ src/shared/Table/Table.mockdata.ts
@@ -0,0 +1,34 @@
+export interface Product {
+    id: number
+    name: string
+    price: number
+    stock: number
+}
+
+const Products: Product[] = [
+    {
+        id: 1,
+        name: 'Cookie',
+        price: 1.25,
+        stock: 20
+    },
+    {
+        id: 2,
+        name: 'Water',
+        price: 0.99,
+        stock: 10
+    },{
+        id: 3,
+        name: 'Detergent',
+        price: 0.75,
+        stock: 54
+    },{
+        id: 4,
+        name: 'Milk 1L',
+        price: 1.50,
+        stock: 30
+    }
+
+]
+
+export default Products;
